0c013a6d4f983a055afa07c66f7a632ff01fca30
KaiyiWing <Zhang.Kaiyi42@gmail.com>
2021-01-22 00:12:24 +0800

    显示字典内容

---
+++ src/components/Loading/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import style from './index.css'
+
+const Loading = (props) => {
+  const { message } = props
+  return (
+    <div className={style.overlay}>
+      <div className={style["child-div"]}>
+        <div className={style["lds-dual-ring"]}></div>
+        <div>{message ? message : 'Loading'}</div>
+      </div>
+    </div >
+  );
+};
+
+Loading.propTypes = {
+};
+
+export default Loading;
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,20 +1,46 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'dva'
 import styles from './IndexPage.css'
 import Word from '../components/Word'
+import Loading from '../components/Loading'
+import { useLoading } from 'react-use-loading'
+
+import cet4Dict from '../assets/CET4_N.json'
+import cet6Dict from '../assets/CET6_N.json'
 
 const words = ['test', 'happy', 'sad', 'a']
 
 function IndexPage() {
   const [order, setOrder] = useState(0)
+  const [{ isLoading, message }, { start, stop }] = useLoading(false)
+  const [dict, setDict] = useState(cet4Dict)
 
   const onFinish = () => {
-    setOrder((order) => (order + 1 < words.length ? order + 1 : order))
+    setOrder((order) => (order + 1 < dict.length ? order + 1 : order))
+  }
+
+  const onChangeDict = (e) => {
+    const value = e.target.value
+    switch (value) {
+      case 'cet4':
+        setDict(cet4Dict)
+        break
+      case 'cet6':
+        setDict(cet6Dict)
+        break
+      default:
+        setDict(cet4Dict)
+    }
   }
 
   return (
     <div className={styles.normal}>
-      <Word key={`${words[order]}`} word={words[order]} onFinish={onFinish} />
+      {isLoading && <Loading />}
+      <select value={dict} onChange={onChangeDict}>
+        <option value="cet4">CET-4</option>
+        <option value="cet6">CET-6</option>
+      </select>
+      <Word key={`${dict[order].name}`} word={dict[order].name} onFinish={onFinish} />
     </div>
   )
 }
@@ -22,3 +48,11 @@ function IndexPage() {
 IndexPage.propTypes = {}
 
 export default connect()(IndexPage)
+
+// const fetchFile = (filename) => {
+//   return fetch(filename)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       console.log('data:', data)
+//     })
+// }
